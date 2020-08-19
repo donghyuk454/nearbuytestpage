c65e5a3d267f894659bf4e61893d46099f6e9070
dongdong <donghyuk454@gmail.com>
2020-08-19 16:51:38 +0900

    1.0.0 기능 구현 마무리

--- post.js
+++ post.js
@@ -1,26 +1,13 @@
-var conf = {
-  server: "http://ec2-3-136-116-6.us-east-2.compute.amazonaws.com:3000",
-  data: {
-    id: "id",
-    value: "value",
-    name: "name"
-  },
+const server = "http://ec2-3-136-116-6.us-east-2.compute.amazonaws.com:3000";
 
-  path_reserve: "/reserve",
-  path_open: "/open",
-  path_close: "/close"
-}
-
-
-function callPost()
+function callPost(data)
 {
-  $.post(conf.server /*+ conf.path_reserve*/, conf.data, function(r){
+  $.post(server + "/db", data, function(r){
     //var o = JSON.parse(r);
     console.log(r);
-    console.log(r.msg);
   });
 }
-
+/*
 function callPost_open(data)
 {
   $.post(conf.server + conf.path_open, data, function(r){
@@ -37,4 +24,4 @@ function callPost_close(data)
     console.log(r);
     console.log(r.msg);
   });
-}
+}*/
