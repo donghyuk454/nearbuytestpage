162664242250f14cbf04af6d4c33acd08e064147
dongdong <donghyuk454@gmail.com>
2020-09-23 00:54:39 +0900

    1.0.1 테스트

--- post.js
+++ post.js
@@ -1,9 +1,9 @@
-const server = "http://183.98.4.54:8887";
+//const server = "http://183.98.4.54:8887";
 
 function callPost()
 {
   var data = {message: "success"};
-  $.post(server, data, function(r){
+  $.post("http://ec2-3-129-5-85.us-east-2.compute.amazonaws.com:3000/open", data, function(r){
     //var o = JSON.parse(r);
     console.log(r);
   });
